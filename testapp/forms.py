4c5936f4b10892564951a781624d8648f7cf2dc9
PrajnaPradhan07 <prajna.mca98@gmail.com>
2026-07-01 19:19:31 +0530

    updated the views and template

--- testapp/forms.py
+++ testapp/forms.py
@@ -2,3 +2,4 @@ from django import forms
 class StudentForm(forms.Form):
     name = forms.CharField()
     marks = forms.IntegerField()
+    rollno = forms.IntegerField()
--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from testapp.forms import StudentForm
 def student_input_view(request):
-    form = StudentForm()
-    return render(request,'testapp/std.html',{'form':form})
+    submitted = False
+    name = ''
+    if request.method == 'POST':
+        form = StudentForm(request.POST) #overidding the empty form
+        if form.is_valid():
+            print("form validation is success and print data")
+            print("Name:",form.cleaned_data['name'])
+            print("Rollno:",form.cleaned_data['rollno'])
+            print("Marks:",form.cleaned_data['marks'])
+            submitted = True
+            name = form.cleaned_data['name']
+    form = StudentForm()  # empty form object to display form to end user
+    return render(request,'testapp/std.html',{'form':form,'submitted':submitted, 'name':name})
